1e61e467b374e22a3e401de1fad75a6a37a53b53
NPavlovaaa <n.pavlova.web@gmail.com>
2023-10-24 16:56:11 +0300

    Add animation to PostList component

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item-list
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item-list
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h3 v-else class="empty">Постов нет</h3>
 </template>
@@ -33,5 +35,21 @@ h3{
 }
 .empty{
   color: teal;
+}
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
 }
 </style>
