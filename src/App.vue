f66cf891913cc29738a95d0d4d44787edee0d152
NPavlovaaa <n.pavlova.web@gmail.com>
2023-10-24 12:13:13 +0300

    Create MyDialog component for addition new post

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <post-form @create="createPost"/>
+    <h1>Страница с постами</h1>
+    <div class="app__btns">
+      <my-button @click="showDialog">
+        Создать пользователя
+      </my-button>
+    </div>
+    <my-dialog v-model:show="visibleDialog">
+      <post-form @create="createPost"/>
+    </my-dialog>
     <post-list
         :posts="posts"
         @remove="removePost"
@@ -11,11 +19,15 @@
 <script>
 import PostForm from "@/components/PostForm";
 import PostList from "@/components/PostList";
+import MyDialog from "@/components/UI/MyDialog";
+import MyButton from "@/components/UI/MyButton";
 
 export default {
   components: {
+    MyDialog,
     PostForm,
-    PostList
+    PostList,
+    MyButton
   },
   data(){
     return{
@@ -23,15 +35,20 @@ export default {
         {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
         {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
         {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
-      ]
+      ],
+      visibleDialog: false
     }
   },
   methods: {
     createPost(post){
-      this.posts.push(post)
+      this.posts.push(post);
+      this.visibleDialog = false;
     },
     removePost(post){
       this.posts = this.posts.filter(item => item.id !== post.id);
+    },
+    showDialog(){
+      this.visibleDialog = true;
     }
   }
 }
@@ -46,5 +63,10 @@ export default {
 }
 .app {
   padding: 50px 100px;
+}
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
